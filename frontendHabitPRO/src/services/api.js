ccfca00a56d39f6f84308ca5e08e5a6b5c06bdb9
vantecc <evansauditore@gmail.com>
2025-04-03 16:41:53 -0300

    requisição do login com o backend resolvida/ agregado funcionado de cadastro e login com google/ agregada a função router para ir pra tela do usuario após logar

--- frontendHabitPRO/src/services/api.js
+++ frontendHabitPRO/src/services/api.js
@@ -1,5 +1,5 @@
 export async function registerUser({ name, email, password }) {
-  const response = await fetch("http://192.168.0.2:8000/api/register/", {
+  const response = await fetch("http://192.168.137.1:8000/api/register/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -12,7 +12,7 @@ export async function registerUser({ name, email, password }) {
 }
 
 export async function loginUser({ email, password }) {
-  const response = await fetch("http://192.168.0.2:8000/api/login/", {
+  const response = await fetch("http://192.168.137.1:8000/api/login/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
